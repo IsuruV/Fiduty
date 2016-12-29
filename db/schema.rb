5fdbcd65a9de88e3ce4a35a9e8230fb869be8b00
IsuruV <isuru260@gmail.com>
2016-12-29 21:25:15 +0000

    c

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20161229201930) do
 
-
   create_table "advisors", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
@@ -75,13 +74,10 @@ ActiveRecord::Schema.define(version: 20161229201930) do
     t.float    "transaction_fee"
     t.float    "fiduty_fee"
     t.float    "weight"
-<<<<<<< HEAD
-=======
     t.float    "trad_price"
     t.float    "ytd"
     t.float    "holding_return"
     t.float    "value"
->>>>>>> ab9241a3437298160a2f8bd501afb048b351a70e
   end
 
   create_table "users", force: :cascade do |t|
