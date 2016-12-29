5294880f3b5a820448bd903dbac207c0a7bcbaf6
Isuru <github email address>
2016-12-29 16:22:28 -0500

    merge

--- app/controllers/user_portfolios_controller.rb
+++ app/controllers/user_portfolios_controller.rb
@@ -18,7 +18,6 @@ class UserPortfoliosController < ApplicationController
 
     def create
       @portfolio = Portfolio.find(params[:portfolio_id])
-      wei = params[:investment_amount].to_i / @portfolio.return_price.to_i
       YahooApi.update_ytd(@portfolio)
       UserPortfolio.create(portfolio: @portfolio, ytd: @portfolio.ytd_raw, user: current_user, inital_investment: params[:investment_amount], shares: params[:shares], investment_date: Time.now, weight: wei )
       respond_to do |format|
